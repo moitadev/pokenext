7f20e089d5d67847e847914a147e3f3173c0ca4b
Moita <katakenshi@icloud.com>
2023-10-10 12:06:09 -0300

    Enables deploy

--- src/components/getColors/getColors.tsx
+++ src/components/getColors/getColors.tsx
@@ -1,5 +1,5 @@
 export default async function getColors() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/pokecolors`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/pokecolors`);
 
   if (!res.ok) {
     throw new Error('Failed to fetch colors');
